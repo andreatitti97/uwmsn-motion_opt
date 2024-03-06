d91065a4f918e0caceaba5e63a2a65435f164ce5
andreatitti97 <andrea.tiranti97@gmail.com>
2024-03-06 19:56:14 +0100

    Implemented First Version of the optimization, TODO: Consider the initialization (how agent behave without the policies of intent of the others

--- src/distr_opt.py
+++ src/distr_opt.py
@@ -24,34 +24,20 @@ spec.loader.exec_module(header)
 t_est_x, t_est_y, s_state_x, s_state_y = [], [], [], []
 t_state = [None, None, None, None]
 s_state = [None, None, None]
-policies_intent = [None, None, None, None]
+policies_intent = [[None], [None], [None], [None]]
 
 # Global Variables
-
 cubicSpline = header.planner
-DT = 15#auvNum*header.config.Td #should be equal more or less to the expected time to perform an estimation
 desired_vel = header.config.AUV_VEL
 
-def update_path(ax, ay, waypoint, s_pose, desired_vel):
-        
-        t_i = s_pose[2]
+def update_path(ax, ay, waypoint, s_pose, desired_vel, DT):
     
-        path = cubicSpline.CubicSpline2D(ax, ay)
-       
-        # Compute the distance travelled according to the new path
-        d_real = path.s[-1]
         a_i = [ax[-1],ay[-1]]
-
-        t_f = t_i+waypoint
-        tmp_x = np.cos(t_f)*desired_vel*DT+a_i[0]
-        tmp_y = np.sin(t_f)*desired_vel*DT+a_i[1]
-        ax.append(tmp_x)
-        ay.append(tmp_y)
-        t_i = t_f
-        a_i = [tmp_x,tmp_y]
+        ax.append(np.cos(s_pose[2]+waypoint)*desired_vel*DT+a_i[0])
+        ay.append(np.sin(s_pose[2]+waypoint)*desired_vel*DT+a_i[1])
         path = cubicSpline.CubicSpline2D(ax, ay)
 
-        return path, d_real, ax, ay, t_f
+        return path, ax, ay
 
 def compute_cost(phi,length_y):
     tmp_phi = np.zeros((length_y,2))
@@ -59,15 +45,14 @@ def compute_cost(phi,length_y):
         a = phi[i]
         tmp_phi[i,:] = [a[0],a[1]]
     PHI = np.dot(np.transpose(tmp_phi),tmp_phi)
-    cost2 = np.linalg.norm(np.linalg.inv(PHI),ord=2)*np.linalg.norm(PHI,ord=2)
-    return cost2
+    return np.linalg.norm(np.linalg.inv(PHI),ord=2)*np.linalg.norm(PHI,ord=2)
 
 class Target():
     def __init__(self, init_state, dt, P=[]):
         'INPUT: estimate state of the target, eventualy associate P for uscented transform'
         self.x = init_state
         self.dt = dt
-        self.cov = P
+        self.P = P
         self.F = np.matrix([[1,0,self.dt,0], # Target State Transition Matrix - CV
                         [0,1,0,self.dt],
                         [0,0,1,0],
@@ -97,26 +82,39 @@ class Estimation():
         self.x = np.dot(np.linalg.pinv(self.phi),tmp_y)
 
 class Simple(pybnb.Problem):
-    def __init__(self,x_hat, s,sensors, cpf_control, ctrl_cmds, ax, ay, d,cov,v_n):
+    def __init__(self, x_hat, s, ctrl_cmds, pi_bar, v_n = 0, cpf_control = []):
         
         inf = float("inf")
-        self.value = header.config.DELTA
-        self.initial_cost = header.config.DELTA
-        self._bound = -inf # initial_cost-100 #lower bound 
-        self.choices = []
-        self._x_hat = x_hat
-        self._s = s
+        
+        # Belief state init
+        sensors = []
+        for i in range(auvNum):
+            sensors.append(header.sensor.Sensor(str(i),1,0,0.000))
         self.sensors = sensors
         self.controller = cpf_control
         self.ctrl_cmds = ctrl_cmds
-        self.cov = cov
-        # Waypoints performed until optimization
-        self.ax = ax 
-        self.ay = ay
-        self.d = d
-        self.v_n = v_n
+        self.pi_bar = pi_bar
+        self._x_hat = x_hat
+        self.P = np.eye(4)
+        self._s = s
+        
+        # Optimization Parameters
+        self.value = header.config.DELTA
+        self.initial_cost = header.config.DELTA
+        self.DT = header.config.DT
+        self._bound = -inf # initial_cost-100 #lower bound 
+        self.choices = []
+        
+        # Variables for path init
+        if v_n != 0:
+            self.v_n = v_n
+        else:
+            self.v_n = header.config.AUV_VEL
+        self.ax = [s_state[0]]
+        self.ay = [s_state[1]]
+        self.d = 0 #distance travelled on the path
 
-    # required methods
+    # Required methods for grapth generation and searching
     def sense(self):
         return pybnb.minimize
 
@@ -128,33 +126,37 @@ class Simple(pybnb.Problem):
 
     def save_state(self, node):
         
-        node.state = (self._x_hat, self._s, self.value, self._bound, self.choices, self.ax, self.ay,self.d)
+        node.state = (self._x_hat, self._s, self.value, self._bound, self.choices, self.ax, self.ay, self.pi_bar)
 
     def load_state(self, node):
 
-        (self._x_hat, self._s, self.value, self._bound, self.choices, self.ax, self.ay,self.d) = node.state
+        (self._x_hat, self._s, self.value, self._bound, self.choices, self.ax, self.ay, self.pi_bar) = node.state
 
     def branch(self): #durante il branch devi calcolare le varie realizzazioni quindi simuli qua
 
-        x_hat, s, cov, ax, ay, d = self._x_hat, self._s, self.cov, self.ax, self.ay, self.d
+        x_hat, s, ax, ay, pi_bar = self._x_hat, self._s, self.ax, self.ay, self.pi_bar
         ax_ = []
         ay_ = []
         for i in range(len(ax)):
             ax_.append(ax[i])
             ay_.append(ay[i])
         for i in range(header.config.U):
-            x, phi, y, s, tmp_ax, tmp_ay, tmp_d = simulation(self.ctrl_cmds[i], x_hat, s, self.sensors, self.controller, ax, ay, d, self.v_n ,cov)
+            x, phi, y, s, tmp_ax, tmp_ay, tmp_pi_bar = simulation(self.ctrl_cmds[i], x_hat, self.P, s, self.sensors, ax, ay, self.v_n, self.DT, pi_bar)
+            
             # Update the sequence of control decisions
             tmp = [self.ctrl_cmds[i]]
             choices = self.choices + tmp
+            
             # If we reached the planning horizon we subtract the DELTA to stop the algorithm and choose only terminal nodes
-            if len(choices) == header.config.M:
+            if len(choices) == header.config.H:
                 self.value = self.value - self.initial_cost ##THIS IS MANDATORY FOR ADDITIVE COST ALONG THE SEQUENCE
                 self._bound = self.value #- cost1 
             father_value = self.value #THIS IS MANDATORY FOR ADDITIVE COST ALONG THE SEQUENC
+            
             # Add the cost of the node to the sequence
             cost = compute_cost(phi,len(y))
             child_value = father_value + cost
+            
             # Branch the tree
             child = pybnb.Node()
             waypoints_x = []
@@ -164,39 +166,88 @@ class Simple(pybnb.Problem):
                 waypoints_y.append(ay_[i])
             waypoints_x.append(tmp_ax)
             waypoints_y.append(tmp_ay)
-            child.state = (x, s, child_value, self._bound, choices, waypoints_x, waypoints_y, tmp_d)
+            child.state = (x, s, child_value, self._bound, choices, waypoints_x, waypoints_y, tmp_pi_bar)
             ax.pop(-1)
             ay.pop(-1)
             yield child
 
-def simulation(control_input, s_pose, target_est, P, sensors, d, v_n):
+def simulation(ctrl_input, x_hat, P, s_pose, sensors, ax, ay, v_n, DT, pi_bar):
     
     # Temporal Variable
     j = 0, 0 #time and counter init
     meas_table = []
 
-    dt = DT
     # Init classes for tracker and target
-    target = Target(target_est, dt, P)
+    target = Target(x_hat, DT, P)
     estimator = Estimation()
 
-    # Compute FINAL agents pose
-    auvs_xy = np.zeros((header.config.N_AUV,2))
-    auvs_theta = np.zeros(header.config.N_AUV)
+    # Initialize data structures for agents state
+    auvs_xy = np.zeros((auvNum,3))
+    ref = np.zeros((auvNum,3)) # [rx,ry,ryaw]^j
+   
+    pi_bar_out = []
+    # Compute the path of the j-th neighbours
+    j_waypoints = []
+
+    for i in range(auvNum):
+        
+        j_pi_bar = pi_bar[i]
+        out = []
+        # TODO ----> ADD INITIALIZATION (how to manage the first optimization without any ctrl_policy)
+        if j_pi_bar[0] != None or i == 0:
+
+            rospy.loginfo('AUV ID %s auv_xy %s j_pi_bar %s pi_bar %s',auvID,auvs_xy,j_pi_bar, pi_bar)
+            auvs_xy[i] = [j_pi_bar[0],j_pi_bar[1],j_pi_bar[2]]
+            j_waypoints = j_pi_bar[3] # to change if more waypoint at this stage
+            ax_j = [auvs_xy[i,0]]
+            ay_j = [auvs_xy[i,1]]
+            if i == 0:
+          # Compute the path of the i-th AUV acoording to the choosen command
+                path_j, ax, ay = update_path(ax, ay, ctrl_input, s_pose, v_n, DT)
+            else:
+                path_j, ax_j, ay_j = update_path(ax_j, ay_j, j_waypoints, auvs_xy[i], v_n, DT)
+            [rx_j, ry_j, ryaw_j, rk, s] = header.utils.calc_spline_course(path_j,DT)
+            
+            ref[i] = [rx_j[-1],ry_j[-1],ryaw_j[-1]]
+            auvs_xy[i] = ref[i]
+            
+            j_pi_bar = np.delete(j_pi_bar,3)
+            tmp1 = ref[i]
+
+            for j in range(len(ref[i])):
+                out.append(tmp1[j])
+            for j in range(len(j_pi_bar)):
+                out.append(j_pi_bar)
+        else:
+            
+            for i in range(len(j_pi_bar)):
+                out.append(None)
+
+        pi_bar_out.append(out)        
+    # Update agents position according to opt policies
+    for i in range(len(auvs_xy)):
+        tmp = pi_bar_out[i] 
+        if tmp[0] != None:
+
+            auvs_xy[i] = ref[i]
+        else: 
+            auvs_xy[i] = [None, None, None]
 
     # Propagate target state estimation
     tmp = np.zeros((4,1))
     for j in range(4):  
         tmp[j]=target.x[j]
     target.x = target.F*tmp
-
-    for j in range(header.config.N_AUV):
-        [measure_, rel_bearing_, meas_pos] = sensors[j].measureBearing(target.x[0],target.x[1],auvs_xy[j],auvs_theta[j])
-        arr = [measure_,meas_pos[0],meas_pos[1]]
-        meas_table.append(arr)
+   
+    # Simulate measurements TODO: (REPRODUCE THE TDMA Sampling!!, not measure everything at the end)
+    for j in range(auvNum):
+        if auvs_xy[i,0] != None:
+            [measure_, rel_bearing_, meas_pos] = sensors[j].measureBearing(target.x[0],target.x[1],[auvs_xy[i,0],auvs_xy[i,1]],auvs_xy[i,2])
+            arr = [measure_,meas_pos[0],meas_pos[1]]
+            meas_table.append(arr)
     estimator.computeState(meas_table)
 
-    return target.x, estimator.phi, estimator.y, s_pose, d
+    return target.x, estimator.phi, estimator.y, s_pose, ax, ay, pi_bar_out
 
 def shutdown_cllbk():
     global auvID, avg_time, avg_nodes
@@ -247,13 +298,13 @@ def listener(auvID,auvNum):
             rospy.Subscriber('/'+str(i+1)+'/rx_ctrl_policy', numpy_msg(Floats), callback_list[i])
 
 def main():
-    global policies_intent, s_state, t_state
+    
     # ROS INIT   
     namespace = rospy.get_namespace()
     params_path = namespace+'auv'
 
     # Get AUV ID and number of vehicles.
-    global auvID, auvNum, avg_nodes, avg_time
+    global auvID, auvNum, avg_nodes, avg_time, policies_intent, s_state, t_state
     auvID = rospy.get_param(params_path+'/auvID')
     auvNum = rospy.get_param(params_path+'/auvNum')
 
@@ -261,33 +312,29 @@ def main():
     rospy.init_node('auv'+str(auvID)) #TO ADD debug prints --> log_level=rospy.DEBUG
 
     # ROS simulation parameters
-    t_scaler = header.config.TIME_SCALER
-
     Hz = 1/(header.config.TIME_STEP) #NB: different from sampling rate for move things, this is ros rate   
     rate = rospy.Rate(Hz)
 
     # Init time variables and counters and lists
     t, count1 = 0,0
     avg_time, avg_nodes = [],[]
-    dt = header.config.TIME_STEP*t_scaler
     old_t_state = [None, None, None, None]
-    # Init publishers and subscribers and sensor objects
-    sensors = []
-    for i in range(auvNum):
-        sensors.append(header.sensor.Sensor(str(i),1,0,0.000))
 
-    pub_ctrl_policy = rospy.Publisher('/'+str(auvID)+'/ctrl_policy',numpy_msg(Floats),queue_size=100)
-    # Compute nodes limit according to RHC with finite memory
-    limit = 0.0
+    # Load simulation parames from config file
+    t_scaler = header.config.TIME_SCALER
+    dt = header.config.TIME_STEP*t_scaler
+    ctrl_choices = header.config.ctrl_cmd
+
+    limit = 0.0# Compute nodes limit according to RHC with finite memory
     for i in range(header.config.H+1):
             limit += header.config.U**i
 
+    # Init publishers and subscribers
+    pub_ctrl_policy = rospy.Publisher('/'+str(auvID)+'/ctrl_policy',numpy_msg(Floats),queue_size=100)
+    
     listener(auvID,auvNum)
     rospy.sleep(1)
-
-    
     while not rospy.is_shutdown():
-
 
         if t_state[0] != old_t_state[0] and t_state[0] != None:
             
@@ -297,23 +344,21 @@ def main():
                 #rospy.loginfo('OPTIMIZATION of AUV ID %s - Policy of intent of AUV %s: %s', auvID, i+1, policies_intent[i])
              
             ######## DO OPTIMIZATION HERE ! ########
-            '''problem = Simple(t_state, s_state, policies_intent,sensors)
+                
+            problem = Simple(t_state, s_state, ctrl_choices, policies_intent)
             solver = pybnb.Solver()
             results = solver.solve(problem,queue_strategy="bound" ,node_limit=limit)#tnode_limit=limi #Uniform cost search con "objective"
-            best_node_states = results.best_node.state #objective_stop=90000,time_limit=5
+            best_node_states = results.best_node.state #objective_stop=90000,time_limit=5 - other queue strategies
             wall_time = results.wall_time
             nodes = results.nodes
             avg_nodes.append(nodes)
             avg_time.append(wall_time)
-            output_policy = best_node_states[4]'''
+            output_policy = best_node_states[4]
             msg = [s_state[0],s_state[1],s_state[2]]
 
             for i in range(header.config.H):
-                msg.append(0)#appendi la sequenza ottimale di controllo
-            
-            output_policy = np.array(msg,dtype=np.float32)
-            
-            pub_ctrl_policy.publish(output_policy)
+                msg.append(output_policy[i])#appendi la sequenza ottimale di controllo
+            pub_ctrl_policy.publish(np.array(msg,dtype=np.float32))
 
         if int(t) == (header.config.TIME_DURATION-1):
             rospy.on_shutdown(shutdown_cllbk)
@@ -328,8 +373,3 @@ def main():
 if __name__ == '__main__':
     
     main()
-
-
-''' TEST ON BnB problem_simplified = Simple(t_est, s_state, DELTA, sensors, cpf_control, ctrl_cmd, P)
-        results_preview = solver.solve(problem,queue_strategy="objective",node_limit=limit)
-        lower_bound = results_preview.objective'''
